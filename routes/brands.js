d0be37607f70f4bc07d400df463ec73b3849faf8
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2021-08-05 11:05:32 +0300

    Change filenames
    If applied, this commit will;
    -‘fetch-instrument-info.js’ filename will be changed to ‘get-instrument-info.js’.
    -‘print-brands.js’ filename will be changed to ‘get-brands.js’.
    -‘filtered.pug’ filename will be changed to ‘filtered-instruments.pug’.

--- routes/brands.js
+++ routes/brands.js
@@ -1,12 +1,12 @@
 const { instrumentDatabase } = require('../database')
-const { printAllBrands } = require('../lib/print-brands')
+const { getAllBrands } = require('../lib/get-brands')
 const router = require('express').Router()
 
 //We pull the brands in the database.
 router.get('/', async (req, res) => {
   const objects = await instrumentDatabase.load()
 
-  const brands = printAllBrands(objects)
+  const brands = getAllBrands(objects)
 
   res.render('brands', { brands })
 })
