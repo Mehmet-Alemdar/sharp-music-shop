94d55c02e956e74058143e854dccaa50eec6405c
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2021-08-24 14:56:05 +0300

    Convert instrument model to mongodb schema

--- models/instrument.js
+++ models/instrument.js
@@ -1,20 +1,13 @@
-const uuid = require('uuid')
+const mongoose = require('mongoose')
 
-class Instrument {
-  constructor(id = uuid.v4(), type, category, kind, brand, model, price, shop) {
-    this.id = id
-    this.type = type
-    this.category = category
-    this.kind = kind
-    this.brand = brand
-    this.model = model
-    this.price = price
-    this.shop = shop
-  }
+const InstrumentSchema = new mongoose.Schema({
+  type: String,
+  category: String,
+  kind: String,
+  brand: String,
+  model: String,
+  price: Number,
+  shop: {},
+})
 
-  static create({ id, type, category, kind, brand, model, price, shop }) {
-    return new Instrument(id, type, category, kind, brand, model, price, shop)
-  }
-}
-
-module.exports = Instrument
+module.exports = mongoose.model('Instrument', InstrumentSchema)
