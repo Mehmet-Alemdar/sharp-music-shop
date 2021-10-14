7c3faafeb0190bac79c149f57cec774590ef1fa7
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2021-10-14 23:25:46 +0300

    Set the customer's phone number as an integer

--- models/customer.js
+++ models/customer.js
@@ -4,7 +4,7 @@ const CustomerSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 2 },
   surname: { type: String, required: true, minLength: 2 },
   email: String,
-  phoneNumber: String,
+  phoneNumber: Number,
   password: String,
   address: String,
   orderHistory: [
