70f3adee7fa75be3064ac2b3c3901c6220975bc4
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2021-10-14 23:29:23 +0300

    Make the fetch-records file work with mongoose

--- fetch-records.js
+++ fetch-records.js
@@ -1,3 +1,4 @@
+require('./mongo-connection')
 const {
   customerService,
   shopService,
@@ -7,23 +8,23 @@ const {
 async function getInfo() {
   try {
     // customer in service
-    const customers = await customerService.findByName('Mehmet')
+    const customers = await customerService.findByName('Teo')
 
     // shop in service
-    const shops = await shopService.findByShopName('Lion Music')
+    const shop = await shopService.findByShopName('Do re')
 
-    const shop = await shopService.load()
+    const shops = await shopService.load()
 
     // instruments in service
     const instruments = await instrumentService.findInstrument()
 
     // instruments of the specified type in the service
-    const types = await instrumentService.findInstrumentByType('Wind')
+    const types = await instrumentService.findInstrumentByType('Strings')
 
     // instruments of the specified model in the service
     const categorys = await instrumentService.findInstrumentByCategory('guitar')
 
-    console.log(customers.orderHistory)
+    console.log(instruments)
   } catch (e) {
     console.log(e)
   }
