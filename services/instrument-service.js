238d98600770f9e2c19e62dc464dee711327e3c3
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2021-10-23 12:09:27 +0300

    Remove the instrument finder functions by instrument information

--- services/instrument-service.js
+++ services/instrument-service.js
@@ -17,45 +17,6 @@ class InstrumentService extends BaseService {
     stock,
     shopId
   ) {
-  async findInstrumentByType(type) {
-    const objects = await this.load()
-
-    return objects.filter((o) => o.type == type)
-  }
-
-  async findInstrumentByCategory(category) {
-    const objects = await this.load()
-
-    return objects.filter((o) => o.category == category)
-  }
-
-  async findInstrumentByKind(kind) {
-    const objects = await this.load()
-
-    return objects.filter((o) => o.kind == kind)
-  }
-
-  async findInstrumentByBrand(brand) {
-    const objects = await this.load()
-
-    return objects.filter((o) => o.brand == brand)
-  }
-
-  //It is used when creating the order.
-  async getFilterInstrument({ type, category, kind, brand, model, shop }) {
-    const objects = await this.load()
-
-    return objects.find(
-      (o) =>
-        o.type == type &&
-        o.category == category &&
-        o.kind == kind &&
-        o.brand == brand &&
-        o.model == model &&
-        o.shop.name == shop
-    )
-  }
-
   async createInstrument(type, category, kind, brand, model, price, shopId) {
     const shop = await shopService.find(shopId)
 
