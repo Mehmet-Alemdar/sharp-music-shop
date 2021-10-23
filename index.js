85d80ddd7ca065d134cb3b5cc9aeee2cca5fc4a0
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2021-10-23 11:58:37 +0300

    Remove the deleted shops.js and instruments.js routers

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const instrumentsRouter = require('./routes/instruments')
 const instrumentRouter = require('./routes/instrument')
-const shopsRouter = require('./routes/shops')
 const brandsRouter = require('./routes/brands')
 const customerRouter = require('./routes/customer')
 const customerLoginRouter = require('./routes/customer-login')
@@ -18,9 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'pug')
 
-app.use('/instruments', instrumentsRouter)
 app.use('/instrument', instrumentRouter)
-app.use('/shops', shopsRouter)
 app.use('/brands', brandsRouter)
 app.use('/customer-login', customerLoginRouter)
 app.use('/customer', customerRouter)
